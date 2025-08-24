72709dc67e8c6c9677a78cf4d7a41fa535d77bbe
MarcosCruzOn <marcoscruzon@gmail.com>
2025-08-24 16:27:08 -0300

    fix: ensure correct product total price is calculated

--- src/app/helpers/product.ts
+++ src/app/helpers/product.ts
@@ -4,15 +4,20 @@ export interface ProductWithTotalPrice extends Product {
   totalPrice: number;
 }
 
-export const computeProductTotalPrice = (
-  product: Pick<Product, "discountPercentage" | "basePrice">,
-): number => {
+export const computerProductTotalPrice = (
+  product: Product,
+): ProductWithTotalPrice => {
   if (product.discountPercentage === 0) {
-    return Number(product.basePrice);
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    };
   }
 
   const totalDiscount =
     Number(product.basePrice) * (product.discountPercentage / 100);
-
-  return Number(product.basePrice) - totalDiscount;
+  return {
+    ...product,
+    totalPrice: Number(product.basePrice) - totalDiscount,
+  };
 };
--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -1,15 +1,17 @@
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/app/helpers/product";
+
 import Image from "next/image";
+import { Badge } from "./ui/badge";
 
 interface ProductItemProps {
-  product: Product;
+  product: ProductWithTotalPrice;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
     <>
       <div className="flex w-[156px] flex-col gap-4">
-        <div className="bg-accent flex h-[176px] w-[156px] items-center justify-center rounded-lg">
+        <div className="bg-accent relative flex h-[176px] w-[156px] items-center justify-center rounded-lg">
           <Image
             src={product.imageUrls[0]}
             alt={product.name}
@@ -19,9 +21,32 @@ const ProductItem = ({ product }: ProductItemProps) => {
             sizes="100vw"
             className="h-auto max-h-[70%] w-auto max-w-[80%]"
           />
+          <Badge className="absolute top-2 left-2">
+            {product.discountPercentage}%
+          </Badge>
         </div>
-        <div className="overflow-hidden text-ellipsis whitespace-nowrap">
-          <p>{product.name}</p>
+
+        <div className="flex flex-col gap-1">
+          <div className="overflow-hidden text-ellipsis whitespace-nowrap">
+            <p>{product.name}</p>
+          </div>
+
+          <div className="flex items-center gap-2">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className="text-l font-semibold">
+                  {product.totalPrice.toFixed(2)}R$
+                </p>
+                <p className="text-xs line-through opacity-75">
+                  {Number(product.basePrice).toFixed(2)}R$
+                </p>
+              </>
+            ) : (
+              <p className="mr-0.5 text-xl font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
+            )}
+          </div>
         </div>
       </div>
     </>
