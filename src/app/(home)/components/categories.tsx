44e528db6bcd4dee08f360283e8059dd96c232b1
MarcosCruzOn <marcoscruzon@gmail.com>
2025-09-10 20:20:50 -0300

    feat:ensure add Component Categories responsive desktop

--- src/app/(home)/components/categories.tsx
+++ src/app/(home)/components/categories.tsx
@@ -4,7 +4,7 @@ import CategoryItem from "./category-item";
 const Categories = async () => {
   const categories = await prismaClient.category.findMany({});
   return (
-    <div className="grid grid-cols-2 gap-x-4 gap-y-1 px-5">
+    <div className="grid grid-cols-2 gap-x-4 gap-y-1 px-5 lg:grid-cols-6">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
